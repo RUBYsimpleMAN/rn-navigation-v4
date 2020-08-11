3b56974544f996f1150ba56447786114e81fcf32
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-08-11 18:00:42 +0300

    2020-08-11_18-00-42_asyncActionCreatePostDONE

--- src/redux/actions/postActions.js
+++ src/redux/actions/postActions.js
@@ -1,13 +1,31 @@
+import * as FileSystem from 'expo-file-system'
+
 import { CREATE_POST, LOAD_POSTS, TOGGLE_BOOKED, REMOVE_POST } from '../types/actionTypes'
 import { ClassDB } from '../../SQLiteDB'
 
 
-export const createPost = post => {
-  post.id = Date.now().toString()
-  return {
-    type: CREATE_POST,
-    payload: post
+export const createPost = post => async dispatch => {
+  const fileNameExists = post.img.split('/').pop()
+  const newPathForPict = FileSystem.documentDirectory + fileNameExists
+  try {
+    await FileSystem.moveAsync({
+      to:   newPathForPict,
+      from: post.img
+    })
   }
+  catch (catchedError) {
+    console.log('Error: ', catchedError)
+  }
+
+  const payload = {...post, img: newPathForPict}
+  const id = await ClassDB.createPost(payload)
+
+  payload.id = id
+
+  dispatch({
+    type: CREATE_POST,
+    payload
+  })
 }
 
 export const loadPosts = () => {
