3b783ae47cfc092e3682b13bf2f86b5e0cb9d090
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-08-11 15:24:05 +0300

    2020-08-11_15-24-05_DatabaseCreatedAndConnected

--- src/SQLiteDB.js
+++ src/SQLiteDB.js
@@ -1,3 +1,20 @@
+import * as SQLite from 'expo-sqlite'
 
+const PostDB = SQLite.openDatabase('post.db')
+
+export class ClassDB {
+  static init() {
+    return new Promise((resolve, reject) => {
+      PostDB.transaction(tx => {
+        tx.executeSql(
+          'CREATE TABLE IF NOT EXISTS postsTable (id INTEGER PRIMARY KEY NOT NULL, text TEXT NOT NULL, img TEXT, date TEXT, booked INT )',
+          [],
+          resolve,
+          (_, error) => reject(error)
+        )
+      })
+    })
+  }
+}
 
 
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,15 @@
 import * as Font from 'expo-font'
+import { ClassDB } from './SQLiteDB'
 
 export async function bootstrap(){
-  await Font.loadAsync({
-    'open-sans-light': require('../assets/fonts/OpenSans-Light.ttf'),
-    'open-sans-normal': require('../assets/fonts/OpenSans-Regular.ttf')
-  })
+  try {
+    await Font.loadAsync({
+      'open-sans-light': require('../assets/fonts/OpenSans-Light.ttf'),
+      'open-sans-normal': require('../assets/fonts/OpenSans-Regular.ttf')
+    })
+    await ClassDB.init()
+    console.log('Database started!')}
+  catch (e) {
+    console.log('Error: ', e)
+  }
 }
